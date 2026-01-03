04f608e484d717bc634d11df5703b34a8aa0904b
besd2025 <besd.promotion1@gmail.com>
2026-01-03 12:45:31 +0200

    1/3

---
+++ app/ui/dashboard/home/campaigne-annee.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Check, ChevronsUpDown, CloudSunRain, Combine } from "lucide-react";
+import { useState } from "react";
+
+const workspaces = [
+  {
+    id: 1,
+    name: "Campagne actuelle",
+    interval: "2025",
+  },
+  {
+    id: 2,
+    name: "Campagne recente",
+    interval: "2024",
+  },
+  {
+    id: 3,
+    name: "Campagne ancienne",
+    interval: "2023",
+  },
+];
+
+export default function CampaigneAnnee() {
+  const [selectedWorkspace, setSelectedWorkspace] = useState(workspaces[0]);
+
+  return (
+    <div className="px-4 lg:px-6">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center gap-2 bg-accent py-2.5 px-3 rounded-lg">
+          <Avatar className="rounded-lg h-8 w-8">
+            <AvatarFallback className="rounded-lg bg-primary text-primary-foreground">
+              <Combine />
+            </AvatarFallback>
+          </Avatar>
+          <div className="text-start flex flex-col gap-1 leading-none">
+            <span className="text-sm leading-none font-semibold truncate max-w-[17ch]">
+              {selectedWorkspace.name}
+            </span>
+            <span className="text-xs text-muted-foreground truncate max-w-[20ch]">
+              {selectedWorkspace.interval}
+            </span>
+          </div>
+          <ChevronsUpDown className="ml-6 h-4 w-4 text-muted-foreground" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-52" align="start">
+          <DropdownMenuLabel>Saisons</DropdownMenuLabel>
+          {workspaces.map((workspace) => (
+            <DropdownMenuItem
+              key={workspace.id}
+              onClick={() => setSelectedWorkspace(workspace)}
+            >
+              <div className="flex items-center gap-2">
+                <Avatar className="rounded-md h-8 w-8">
+                  <AvatarFallback className="rounded-md bg-primary/10 text-foreground">
+                    {workspace.name[0]}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <span>{workspace.name}</span>
+                  <span className="text-xs text-muted-foreground">
+                    {workspace.interval}
+                  </span>
+                </div>
+              </div>
+              {selectedWorkspace.id === workspace.id && (
+                <Check className="ml-auto" />
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
--- app/ui/dashboard/home/index.jsx
+++ app/ui/dashboard/home/index.jsx
@@ -4,12 +4,14 @@ import { ChartPieSdlCtActive } from "./charts/sdl-ct-active";
 import { ChartLineAchats } from "./charts/sdl-ct-achats";
 import { StockSummaryCard } from "./stock-card";
 import { KPIGrid } from "./kpi-stats";
+import CampaigneAnnee from "./campaigne-annee";
 
 function DashboardContainer() {
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 ">
+          <CampaigneAnnee />
           <SectionCards />
 
           <div className="px-4 lg:px-6 grid grid-cols-5 gap-4">
