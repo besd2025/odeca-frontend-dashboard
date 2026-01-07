aa65daa6df04f928c2af002f5238e2e57938ef83
JMVianney1 <ndayiragijejmv1@gmail.com>
2026-01-07 14:12:56 +0200

    new commit 07-01-2026 14:12

--- app/ui/auth/login-form.jsx
+++ app/ui/auth/login-form.jsx
@@ -80,15 +80,14 @@ export function LoginForm({ className, ...props }) {
       }
       setLoading(true);
       const data = await response.json();
-      console.log("Login successful:", data);
-      document.cookie = `accessToken=${data.access}; path=/; max-age=3600; secure`;
-      localStorage.setItem("accessToken", data.access);
       const user = DecodeToJwt(data.access);
       if (
         user?.category === "Admin" ||
         //user?.category === "Anagessa" ||
         user?.category === "General"
       ) {
+        document.cookie = `accessToken=${data.access}; path=/; max-age=3600; secure`;
+        localStorage.setItem("accessToken", data.access);
         router.push("/odeca-dashboard/home");
         // } else if (user?.category === "Communal") {
         //   router.push("/municipal/cultivators");
