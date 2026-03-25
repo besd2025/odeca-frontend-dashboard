46d6f35cde935858045b84c6a6d4ebfc4646d483
JMVianney1 <ndayiragijejmv1@gmail.com>
2026-03-25 17:51:01 +0200

    new commit 25-03-2026 17h50

--- app/ui/dashboard/stocks/achats/achats_data.jsx
+++ app/ui/dashboard/stocks/achats/achats_data.jsx
@@ -5,8 +5,28 @@ import AchatsListTable from "./achats-list-table";
 import { ChartColumn, Edit, List } from "lucide-react";
 import AchatsListTableEdition from "./edition/achats-list-table";
 import { Badge } from "@/components/ui/badge";
-
+import { fetchData } from "@/app/_utils/api";
+import { UserContext } from "@/app/ui/context/User_Context";
 function AchatsData() {
+  const [totalCount, setTotalCount] = React.useState(0)
+  React.useEffect(() => {
+    const getAchats = async () => {
+      try {
+        const response = await fetchData("get", "cafe/achat_modification/", {
+          params: {},
+          additionalHeaders: {}
+        });
+        setTotalCount(response?.count || 0);
+      } catch (error) {
+        console.error("Error fetching individual achats:", error);
+      } finally {
+
+      }
+    };
+
+    getAchats();
+  }, []);
+  const user = React.useContext(UserContext)
   return (
     <div className="p-4">
       <Tabs defaultValue="list" className="w-full">
@@ -15,10 +35,14 @@ function AchatsData() {
             <List />
             <span>Liste</span>
           </TabsTrigger>
-          <TabsTrigger value="details">
-            <Edit />
-            <span>Edition  <Badge className="min-w-5.5 px-1 ml-2">10</Badge></span>
-          </TabsTrigger>
+          {(user?.session?.category === "Admin" || user?.session?.category === "Cafe_ODECA") && (
+            <TabsTrigger value="details">
+              <Edit />
+              <span>
+                Edition <Badge className="min-w-5.5 px-1 ml-2">{totalCount || 0}</Badge>
+              </span>
+            </TabsTrigger>
+          )}
         </TabsList>
         <TabsContent value="list">
           <h1 className="text-2xl font-semibold m-2">Liste des achats</h1>
