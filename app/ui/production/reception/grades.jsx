e0f403b3ea82707149cfccd5dff94a78da9701b1
JMV <ndayiragijejmv1@gmail.com>
2026-06-09 18:24:10 +0200

    new

--- app/ui/production/reception/grades.jsx
+++ app/ui/production/reception/grades.jsx
@@ -177,7 +177,6 @@ export default function Grades() {
             nombre_sac: formData.sacsCount,
             comfiramtion_status: "CONFIRMEE"
         }
-        console.log(dataform)
         if (!formData.gradeId || !formData.humidite || !formData.poidsBrut || !formData.poidsTare || !formData.sacsCount) {
             toast.error("Veuillez remplir tous les champs");
             setLoading(false);
